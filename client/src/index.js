b786db6f022e37221090483ba5ee4fc8c5c83fee
Nita Rai <rnita0891@gamil.com>
2023-08-01 14:40:14 +0545

    button component

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ButtonComponent from './components/Button';
+import Title from './Title';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ButtonComponent />
+    <Title />
   </React.StrictMode>
 );
